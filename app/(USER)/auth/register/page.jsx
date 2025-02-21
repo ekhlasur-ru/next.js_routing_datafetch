be990a698655b5a965006f412209fe9a9cb8238b
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-02-21 23:30:47 +0600

    add user route

---
+++ app/(USER)/auth/register/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="h-screen flex justify-center items-center text-8xl">
+      Register Page
+    </div>
+  );
+}
+
+export default page;
